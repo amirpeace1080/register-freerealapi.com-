56fe7911c845591eb9c59bcf51cd359738e95676
amirpeace1080 <amirpeace1080@gmail.com>
2021-12-03 13:49:21 +0330

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- <h4>token =  {{ this.$store.state.token }} </h4> -->
+    <form>
+      <label>Name</label>
+      <input type="text" v-model="name" />
+      <br />
+      <label>Email</label>
+      <input type="email" v-model="email" />
+      <br />
+      <label>Password</label>
+      <input type="text" v-model="password" />
+      <br /><br />
+      <button @click="register">Register</button>
+    </form>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data(){
+    return {
+      name: null,
+      email: null,
+      password: null
+    }
+  },
+ mounted(){
+   axios({
+     url: "https://api.freerealapi.com/auth/register",
+     method: "POST",
+     headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: "amir",
+      email: 'amirpeac@gmail.com',
+      password: "Kavan@joke23424",
+    })
+  })
+  .then((response) => response.json())
+  .then((json) => console.log(json))
+ }
+}
+</script>
+
+<style></style>
