0f9913e73247175ac794993d9ef96941b0bc1813
amirpeace1080 <amirpeace1080@gmail.com>
2021-12-05 11:31:59 +0330

    status 200 in console

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,46 +2,64 @@
   <div class="about">
     <!-- <h4>token =  {{ this.$store.state.token }} </h4> -->
     <form>
-      <label>Name</label>
+      <label>Name: </label>
       <input type="text" v-model="name" />
       <br />
-      <label>Email</label>
+      <label>Email: </label>
       <input type="email" v-model="email" />
       <br />
-      <label>Password</label>
+      <label>Password: </label>
       <input type="text" v-model="password" />
       <br /><br />
-      <button @click="register">Register</button>
+      <button @click="aaa">Register</button>
     </form>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  data(){
+  data() {
     return {
       name: null,
       email: null,
-      password: null
-    }
+      password: null,
+    };
   },
- mounted(){
-   axios({
-     url: "https://api.freerealapi.com/auth/register",
-     method: "POST",
-     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      name: "amir",
-      email: 'amirpeac@gmail.com',
-      password: "Kavan@joke23424",
-    })
-  })
-  .then((response) => response.json())
-  .then((json) => console.log(json))
- }
-}
+  methods: {
+    aaa() {
+      let name = this.name;
+      let email = this.email;
+      let password = this.password;
+      axios({
+        url: "https://api.freerealapi.com/auth/register",
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+        }),
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    },
+  },
+  mounted() {
+    //  axios({
+    //    url: "https://api.freerealapi.com/auth/register",
+    //    method: "POST",
+    //    headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({
+    //     name: this.name,
+    //     email: this.email,
+    //     password: this.password,
+    //   })
+    // })
+    // .then((response) => response.json())
+    // .then((json) => console.log(json))
+  },
+};
 </script>
 
 <style></style>
